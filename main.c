c35fef2350ec0ec206c276fa4404b8a0610f4328
Jjb3tee3 <funny0036@gmail.com>
2014-02-05 21:40:04 +0000

    Added probe packet listener.

--- main.c
+++ main.c
@@ -15,6 +15,36 @@ void usage(char *argv[]) {
 	printf("\t%s\n", argv[0]);
 }
 
+int is_probe_req(lorcon_packet_t *packet) {
+	int ret, i;
+	char ssid[256];
+	char mac[MAC_LEN];
+
+	if((packet->packet_header[0] == 0x40) && (packet->packet_header[PSSID_OFFSET-1] < 255) && (packet->packet_header[PSSID_OFFSET-1] != 0)) {
+		for(i=0; i<packet->packet_header[PSSID_OFFSET-1]; i++) {
+			ssid[i] = packet->packet_header[PSSID_OFFSET + i];
+		}
+
+		ssid[packet->packet_header[PSSID_OFFSET-1]] = '\0';
+	
+		ret = strncmp(ssid, ap_info.ssid, packet->packet_header[PSSID_OFFSET-1]);
+
+		if(ret == 0) {
+			// Grab mac to make sure we can filter results...
+			ap_info.got_probe_req = 1;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+void probe_listen(lorcon_t *context, lorcon_packet_t *packet, u_char *user) {
+	if(is_probe_req(packet) && ap_info.got_probe_req == 1) {
+		printf("[+] Detected probe request!\n");
+	}
+}
+
 int main(int argc, char **argv) {
 	char *iface;
 	int c, ret;
